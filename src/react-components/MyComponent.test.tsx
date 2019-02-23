bbab2ee2ed56a4aa90729330fd3a1be22018b866
Brian Pfeil <brian.pfeil@gmail.com>
2019-02-23 13:38:01 -0500

    updated MyComponent test

--- src/react-components/MyComponent.test.tsx
+++ src/react-components/MyComponent.test.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import MyComponent from "./MyComponent";
 import * as renderer from "react-test-renderer";
 import "jest-dom/extend-expect";
-import {
-  render,
-  fireEvent,
-  cleanup,
-  waitForElement,
-  getByText
-} from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 
-test("Link changes the class when hovered", () => {
+test("MyComponent works", async () => {
   const component = renderer.create(<MyComponent title="Hello World" />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
-  const { getByText } = render(<MyComponent title="Hello World" />);
-
+  const { getByText, getByLabelText } = render(
+    <MyComponent title="Hello World" />
+  );
   expect(getByText(/hello/i)).toHaveTextContent("Hello World");
+
+  fireEvent.click(getByText(/click/i));
+  expect(document.querySelector("[name=count]")).toHaveAttribute("value", "1");
 });
--- src/react-components/MyComponent.tsx
+++ src/react-components/MyComponent.tsx
@@ -1,9 +1,31 @@
 import * as React from "react";
-export default class MyComponent extends React.Component<{title: string}> {
+export default class MyComponent extends React.Component<
+  { title: string },
+  { count: number }
+> {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  handleClick(event) {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
+        <div>
+          <label htmlFor="count">Count</label>
+          <input
+            type="text"
+            name="count"
+            readOnly
+            value={`${this.state.count}`}
+          />
+          <button onClick={this.handleClick.bind(this)}>click</button>
+        </div>
       </div>
     );
   }
